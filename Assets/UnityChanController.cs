9e4f9cc4527329e7f72aa46da0f887a550f50bb3
superjimy <tadashi.azegami@gmail.com>
2018-01-06 17:00:48 +0900

    StepAsideUnityChan
    ユニティちゃん３Dゲーム

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+	//アニメーションするためのコンポーネントを入れる
+	private Animator myAnimator;
+	//Unityちゃんを移動させるコンポーネントを入れる（追加）
+	private Rigidbody myRigidbody;
+	//前進するための力（追加）
+	private float forwardForce = 800.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Animatorコンポーネントを取得
+		this.myAnimator = GetComponent<Animator>();
+
+		//走るアニメーションを開始
+		this.myAnimator.SetFloat ("Speed", 1);
+
+		//Rigidbodyコンポーネントを取得（追加）
+		this.myRigidbody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Unityちゃんに前方向の力を加える（追加）
+		this.myRigidbody.AddForce (this.transform.forward * this.forwardForce);
+		
+	}
+}
