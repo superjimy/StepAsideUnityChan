f8fc693ed3d7887a984caa2e6e18db7b62c4c812
superjimy <tadashi.azegami@gmail.com>
2018-01-07 09:25:22 +0900

    lesson6
    unityちゃん左右に移動、ジャンプができるようになった

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class UnityChanController : MonoBehaviour {
 	//アニメーションするためのコンポーネントを入れる
 	private Animator myAnimator;
-	//Unityちゃんを移動させるコンポーネントを入れる（追加）
+	//Unityちゃんを移動させるコンポーネントを入れる
 	private Rigidbody myRigidbody;
-	//前進するための力（追加）
+	//前進するための力
 	private float forwardForce = 800.0f;
+	//左右に移動するための力
+	private float turnForce = 500.0f;
+	//ジャンプするための力（追加）
+	private float upForce = 500.0f;
+	//左右に移動できる範囲
+	private float movableRange = 3.4f;
 
 
 	// Use this for initialization
@@ -20,7 +26,7 @@ public class UnityChanController : MonoBehaviour {
 		//走るアニメーションを開始
 		this.myAnimator.SetFloat ("Speed", 1);
 
-		//Rigidbodyコンポーネントを取得（追加）
+		//Rigidbodyコンポーネントを取得
 		this.myRigidbody = GetComponent<Rigidbody>();
 		
 	}
@@ -28,8 +34,28 @@ public class UnityChanController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//Unityちゃんに前方向の力を加える（追加）
+		//Unityちゃんに前方向の力を加える
 		this.myRigidbody.AddForce (this.transform.forward * this.forwardForce);
-		
+
+		//Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
+		if (Input.GetKey (KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x) {
+			//左に移動
+			this.myRigidbody.AddForce (-this.turnForce, 0, 0);
+		} else if (Input.GetKey (KeyCode.RightArrow) && this.transform.position.x < this.movableRange) {
+			//右に移動
+			this.myRigidbody.AddForce (this.turnForce, 0, 0);
+		}
+		//jumpステートの場合はjumpにfalseをセットする（追加）
+		if (this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("jump")) {
+			this.myAnimator.SetBool ("jump", false);
+		}
+
+		//ジャンプしていない時にスペースが押されたらジャンプする（追加）
+		if (Input.GetKeyDown (KeyCode.Space) && this.transform.position.y < 0.5f) {
+			//ジャンプアニメを再生（追加）
+			this.myAnimator.SetBool ("jump", true);
+			//Unityちゃんに上方向の力を加える（追加）
+			this.myRigidbody.AddForce (this.transform.up * this.upForce);
+		}
 	}
 }
